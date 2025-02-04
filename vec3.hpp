8cfb16e677b7c0a57f55e284ed01bb9acd07fb8c
sofia-i <s.iannicelli@byu.net>
2025-02-04 14:49:25 -0700

    Fix bracket access

--- vec3.hpp
+++ vec3.hpp
@@ -41,8 +41,8 @@ public:
     T z() const { return e[2]; }
 
     vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    double operator[](int i) const { return e[i]; }
-    // double& operator[](int i) { return e[i]; }
+    T operator[](int i) const { return e[i]; }
+    T& operator[](int i) { return e[i]; }
 
     // compound operators
     vec3& operator+=(const vec3<T> &v) {
