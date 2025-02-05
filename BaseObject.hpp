5b6431c71bc285270c8540c4e14d8dae5e74384d
sofia-i <s.iannicelli@byu.net>
2025-02-04 19:56:51 -0700

    Clear out BaseObject

--- BaseObject.hpp
+++ BaseObject.hpp
@@ -18,41 +18,6 @@
 #include "Material.h"
 
 class BaseObject {
-public:
-    BaseObject(const std::shared_ptr<Material>& material, std::string description) :
-                material(material), description(std::move(description)) { }
-
-    virtual ~BaseObject() = default; // I. destructor
-    BaseObject(const BaseObject& other) = default; // II. copy constructor
-    BaseObject& operator=(const BaseObject& other) = default; // III. copy assignment
-    BaseObject(BaseObject&& other) noexcept = default;// IV. move constructor
-    BaseObject& operator=(BaseObject&& other) noexcept = default; // V. move assignment
-
-    virtual double findRayObjectIntersection(Ray ray) = 0;
-    virtual double findRayObjectIntersection(Ray ray, vec3<double>& intersectNormal, bool& backFace) = 0;
-
-    std::shared_ptr<Material> mat() const { return material; }
-    std::shared_ptr<Material> getMaterial() const { return material; }
-    std::string getDescription() const { return description; }
-
-    friend std::ostream& operator<<(std::ostream& os, const BaseObject& obj) {
-        os << obj.toString();
-        return os;
-    }
-    
-    virtual std::string toString() const {
-        std::string s;
-        std::stringstream ss(s);
-        
-        // ss << getDescription() << std::endl;
-        ss << material->toString() << std::endl;
-        
-        return ss.str();
-    }
-
-protected:
-    std::shared_ptr<Material> material;
-    std::string description;
     
 };
 
